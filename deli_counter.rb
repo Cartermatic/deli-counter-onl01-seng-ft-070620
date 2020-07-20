d297b70e971270a0b526ccb67481d32739b283c7
Carter Parrett <cparrett94@hotmail.com>
2020-07-20 17:08:47 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,29 +1,66 @@
 katz_deli = []
 
 def line(katz_deli)
-  if katz_deli.any? == false
+  people_in_line_formatted = []
+  
+  katz_deli.each_with_index do |person, place_in_line|
+    people_in_line_formatted << "#{place_in_line + 1}. #{person}"
+  end
+  
+  if katz_deli.length == 0
     puts "The line is currently empty."
-  else 
-    line_array = []
-    counter = 1
-    katz_deli.each do |guest|
-      line_array.push("#{counter}. #{guest}")
-      counter += 1
-    end
-    puts "The line is currently: #{line_array.join(" ")}"
+  else
+    puts "The line is currently: #{people_in_line_formatted.join(" ")}"
   end
 end
 
 def take_a_number(katz_deli, name)
-  katz_deli.push(name)
-  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
-end
-
-def now_serving(array)
-  if array.empty?
-    puts "There is nobody waiting to be served!"
+  if katz_deli.length == 0
+    katz_deli << name
+    puts "Welcome, #{name}. You are number 1 in line."
   else
-    puts "Currently serving #{array[0]}."
-    array.shift
+    katz_deli << name
+    puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
   end
 end
+
+def now_serving(katz_deli)
+  if katz_deli.length == 0 
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{katz_deli.first}."
+    katz_deli.shift
+  end
+end
+  
+  
+  
+#   katz_deli = []
+
+# def line(katz_deli)
+#   if katz_deli.any? == false
+#     puts "The line is currently empty."
+#   else 
+#     line_array = []
+#     counter = 1
+#     katz_deli.each do |guest|
+#       line_array.push("#{counter}. #{guest}")
+#       counter += 1
+#     end
+#     puts "The line is currently: #{line_array.join(" ")}"
+#   end
+# end
+
+# def take_a_number(katz_deli, name)
+#   katz_deli.push(name)
+#   puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+# end
+
+# def now_serving(array)
+#   if array.empty?
+#     puts "There is nobody waiting to be served!"
+#   else
+#     puts "Currently serving #{array[0]}."
+#     array.shift
+#   end
+# end
